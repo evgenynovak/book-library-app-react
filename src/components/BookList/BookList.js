6276b3374a17f5e5aa4002a1c1c246211257fdcb
evgenynovak <evnovak171@mail.ru>
2019-11-29 13:25:20 +0300

    add BookList component

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import scss from './BookList.module.scss'
 
 class BookList extends React.Component {
  render() {
    return (
-      <div>BookList</div> 
-
+      <div className={scss.BookList}>
+        <ui className={scss.BookList__items}>
+          <li className={scss.BookList__item}>Название книги 1</li>
+          <li className={scss.BookList__item}>Название книги 2</li>
+          <li className={scss.BookList__item}>Название книги 3</li>
+          <li className={scss.BookList__item}>Название книги 4</li>
+          <li className={scss.BookList__item}>Название книги 5</li>
+          <li className={scss.BookList__item}>Название книги 6</li>
+          <li className={scss.BookList__item}>Название книги 7</li>
+        </ui>
+      </div>
    )
  }
 }
