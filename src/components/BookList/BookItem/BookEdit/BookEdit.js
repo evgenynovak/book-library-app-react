df807405c12f25a9a218f7fd5f1ee3f1756b207c
evgenynovak <evnovak171@mail.ru>
2019-12-05 22:24:26 +0300

    BookEdit: add input chenge and styles

--- src/components/BookList/BookItem/BookEdit/BookEdit.js
+++ src/components/BookList/BookItem/BookEdit/BookEdit.js
@@ -1,20 +1,39 @@
 import React from 'react'
 import scss from './BookEdit.module.scss'
 
-class BookOptions extends React.Component {
+class BookEdit extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+        titleBook : this.props.data.titleBook
       };
   }
 
+  handleSubmitFormEdit = (e) => {
+
+  }
+
+  handleChangeInputTitle = (e) => {
+    this.setState({titleBook: e.target.value});
+  }
+
  render() {
+  const data = this.props.data
     return (
-        <div>
-          test
-        </div>
+        <form className={scss.form} onSubmit={this.handleSubmitFormEdit}>
+            <label className={scss.formLabel}>Название книги</label>
+            <input 
+              className={scss.formInputTitle}
+              id='titleBook'
+              type='text' 
+              value={this.state.titleBook}
+              onChange={this.handleChangeInputTitle}
+              placeholder='Введите название книги'
+            />
+        <input className={scss.formSubmit} type='submit' name='submit' value="Сохранить"/>
+      </form>
     )
   }
  }
  
- export default BookOptions
+ export default BookEdit
