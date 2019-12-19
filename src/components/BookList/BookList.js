a16f6e6f12d1f849120cc5ef85cb731935ca0105
evgenynovak <evnovak171@mail.ru>
2019-12-19 22:26:52 +0300

    BookList: remade on hooks

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -1,38 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BookItem from './BookItem/BookItem'
 import scss from './BookList.module.scss'
 
-class BookList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isRemoveItem: false  
-    }
-  }
+function BookList(props)  {
+  const [isRemoveItem, setIsRemoveItem] = useState(false);
+  
 
-  handleRemoveItem = (id) => {
-    const {data} = this.props
+  const handleRemoveItem = (id) => {
+    const {data} = props
     const index = data.findIndex(item => item.id === id);
       if (index > -1) {
         data.splice(index, 1);
-        this.setState( { isRemoveItem: true} )
+        setIsRemoveItem(!isRemoveItem);
       }
   }
 
-  renderBookItems = () => {
-    const {data} = this.props
-    return data.map(item => <BookItem key={item.id} data={item} tags={item.tags} onRemoveItem={this.handleRemoveItem}/>)
+  const renderBookItems = () => {
+    const {data} = props
+    return data.map(item => <BookItem key={item.id} data={item} tags={item.tags} onRemoveItem={handleRemoveItem}/>)
   }
 
- render() {
-   return (
+  return (
       <div className={scss.bookList}>
         <ul className={scss.items}>
-        {this.renderBookItems()}
+        {renderBookItems()}
         </ul>
       </div>
-   )
- }
+   );
 }
 
 export default BookList
