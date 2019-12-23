a1bee73cdda5145f6a3c4ea6cce01c29b0db6d0f
evgenynovak <evnovak171@mail.ru>
2019-12-23 23:34:58 +0300

    App: remade on hooks

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AddBook from './components/AddBook/AddBook'
 import BooksList from './components/BookList/BookList'
 import scss from './App.module.scss'
 import stateBook from './bookbase.js'
 
-class App extends React.Component {
-  state = {
-    books: stateBook.booksData,
-    tags: stateBook.tagsData
-  }
-  debuger;
+function App() {
+  const [books, setBooks] = useState(stateBook.booksData);
+  const [tags, setTags] = useState(stateBook.tagsData);
 
-  handleAddBook = (data) => {
+  const handleAddBook = (data) => {
     data.id = Date.now();
-    const nextBook = [data, ...this.state.books]
-    this.setState({ books: nextBook })
+    const nextBook = [data, ...books]
+    setBooks(nextBook)
   }
 
- render() {
-  console.log(this.state.books)
-   return (
+ return (
     <div className={scss.app}>
-    <AddBook onAddBook={this.handleAddBook}/>
-    <BooksList data={this.state.books}/>
+    <AddBook onAddBook={handleAddBook}/>
+    <BooksList data={books}/>
     </div>
-   )
- }
+ )
 }
 
 export default App
