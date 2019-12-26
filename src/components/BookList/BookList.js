df5595d3f559e1b03b95db972920bc05ad283bb6
evgenynovak <evnovak171@mail.ru>
2019-12-26 21:03:02 +0300

    BookList: added propTypes

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 import BookItem from './BookItem/BookItem'
 import scss from './BookList.module.scss'
 
 function BookList(props)  { 
-  
+
   const renderBookItems = () => {
     const {data, onRemoveItem} = props
     return data.map(item => <BookItem key={item.id} data={item} tags={item.tags} onRemoveItem={onRemoveItem}/>)
@@ -17,5 +18,9 @@ function BookList(props)  {
       </div>
    );
 }
+
+BookList.propTypes = {
+  data: PropTypes.array
+};
 
 export default BookList
