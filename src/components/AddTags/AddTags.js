cb8942b2fe39b737a0dac2d71c291c4b1ec1f5c5
evgenynovak <evnovak171@mail.ru>
2020-04-09 01:42:34 +0300

    add new tags

---
+++ src/components/AddTags/AddTags.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import scss from './AddTags.module.scss'
+
+function AddTags(props) {
+
+  const [buttonEditTagsIsPressed, setButtonEditTagsIsPressed] = useState(false);
+  const [tagName, setTagName] = useState('');
+
+  let {onAddTag} = props
+
+  let handleOpenClose = () => {
+    setTagName('')
+    setButtonEditTagsIsPressed(!buttonEditTagsIsPressed)
+  }
+
+  let handleSubmitFormAddTags = () => {
+    const data = {
+            tagId: Date.now(),
+            tagText: tagName,
+            isChecked: false
+          }
+    onAddTag(data)
+    setTagName('')
+  }
+
+  return (
+    <div className={scss.addTags}>
+    <button
+      type='button'
+      onClick={handleOpenClose}>
+      Теги... {buttonEditTagsIsPressed}
+    </button>
+
+    {buttonEditTagsIsPressed && 
+      <div>
+        <form className={scss.form}>
+          <input 
+            className={scss.formInputTitle}
+            type='text'
+            value={tagName}
+            onChange={(e) => setTagName(e.target.value)}
+            placeholder='Введите название тега'
+          />
+        <button className={scss.formSubmit} type='button' onClick={handleSubmitFormAddTags}>Сохранить</button> 
+        <button className={scss.formSubmit} type='button' onClick={handleOpenClose}> X </button>
+        </form>
+      </div>
+    }
+    </div>
+  )
+
+}
+
+export default AddTags
+
+
