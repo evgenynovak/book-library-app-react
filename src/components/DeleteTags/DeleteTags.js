e0fe8f5709d2a9d583e74f3936bb9611b9bcecaa
evgenynovak <evnovak171@mail.ru>
2020-04-21 22:39:24 +0300

    add start files component DeleteTags

---
+++ src/components/DeleteTags/DeleteTags.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import scss from './DeleteTags.module.scss'
+
+function DeleteTags(props) {
+
+}
+
+/*
+DeleteTags.propTypes = {
+  onAddTag: PropTypes.func
+};
+*/
+
+export default DeleteTags
